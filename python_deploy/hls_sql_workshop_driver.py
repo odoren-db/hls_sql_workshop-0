8d65e37e88589574d004eb8ab42d13cd8676f4b6
ddavisdbrx <ddavisdbrx>
2024-11-30 03:03:47 +0000

    add img

--- python_deploy/hls_sql_workshop_driver.py
+++ python_deploy/hls_sql_workshop_driver.py
@@ -11,13 +11,19 @@
 # MAGIC       - The catalog, schema, and volume names that you select will be automatically created in the generated workflow, provided you have the appropriate permissions to create these objects.
 # MAGIC   3. **THIRD** execute this notebook. Once it executes successfully, it will generate a workflow that will: 
 # MAGIC         - setup UC (e.g. catalog, schemas, etc.) 
-# MAGIC         - ingest CMS data 
+# MAGIC         - copy CMS files to your volume
 # MAGIC         - create and execute the DLT pipeline that creates the bronze/silver/gold tables
 # MAGIC         - train and register an ML model
 # MAGIC         - create an online table
 # MAGIC         - create a serving endpoint
-# MAGIC   4. **FOURTH** once this notebook finishes executing, **you will need to manually run the workflow that it generates**. The name of the workflow can be found in the output of the last cell.
-# MAGIC   ![](python_deploy/img/workflow_cell_output.png)
+# MAGIC   4. **FOURTH** once this notebook finishes executing, **you will need to manually run the workflow that it generates**. The name of the workflow will be in the format *\<first_name>_\<last_name>_hls_sql_workshop*. 
+# MAGIC   The name of the workflow, ID, and URL can be found in the output of the last cell.
+# MAGIC   5. **FIFTH** Once your workflow executes successfully, your dataset will be ready to run the HLS SQL Workshop.
+# MAGIC
+# MAGIC <img src="https://github.com/ddavisdbrx/hls_sql_workshop/blob/main/python_deploy/img/workflow_cell_output.jpg?raw=true" width="200"/>
+# MAGIC
+# MAGIC
+# MAGIC If you run into any issues, please contact Dan Davis (dan.davis@databricks.com)
 
 # COMMAND ----------
 
@@ -67,7 +73,6 @@ if compute_type == "classic":
     node_type_id = "Standard_D8ads_v5"
 else:
   node_type_id = None
-
 
 print(f'Node type ID: {node_type_id}')
 
